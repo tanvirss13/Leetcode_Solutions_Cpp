e5d9e9e84a23e607902fd31a671fddd8e9597d5e
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2022-10-13 12:54:52 -0400

    Create Find_Numbers_with_Even_Number_of_Digits.cpp
    Given an array nums of integers, return how many of them contain an even number of digits.
    
    
    Input: nums = [12,345,2,6,7896]
    Output: 2
    Explanation: 
    12 contains 2 digits (even number of digits). 
    345 contains 3 digits (odd number of digits). 
    2 contains 1 digit (odd number of digits). 
    6 contains 1 digit (odd number of digits). 
    7896 contains 4 digits (even number of digits). 
    Therefore only 12 and 7896 contain an even number of digits.
    
    
    Input: nums = [555,901,482,1771]
    Output: 1 
    Explanation: 
    Only 1771 contains an even number of digits.
    
    
    Constraints:
    
    1 <= nums.length <= 500
    1 <= nums[i] <= 105

---
+++ LeetcodeLearnSolutions/Array/Find_Numbers_with_Even_Number_of_Digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int count = 0;
+        for (int i=0; i<nums.size();i++){
+            if((std::to_string(nums[i]).size())%2==0){
+                count += 1;
+            }
+        }
+        return count;
+    }
+};
