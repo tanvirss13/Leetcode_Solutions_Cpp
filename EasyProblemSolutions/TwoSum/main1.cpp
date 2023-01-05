0f76c3dd2cdaef9bcfc9a90fc752355d185198be
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2023-01-05 17:53:03 -0500

    Create main1.cpp

---
+++ EasyProblemSolutions/TwoSum/main1.cpp
@@ -0,0 +1,19 @@
+include <iostream>
+
+//This is for 0(n^2) time | 0(1) space
+#include <vector>
+using namespace std;
+
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+    for(int i = 0; i < array.size()-1; i++){
+        int x = array[i];
+        for(int j = i+1; j < array.size(); j++){
+            int y = array[j];
+            if (x + y == targetSum){
+                return vector<int>{x, y};
+            }
+        }
+    }
+  return {};
+}
